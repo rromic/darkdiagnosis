483791741221d3758cdbf4022b4e72d019328c8b
croraf <vrafaeli@msn.com>
2020-04-15 22:49:29 +0200

    Fix HomeRouter to acount obsolete paths

--- frontend/src/routes/Home/HomeRouter.js
+++ frontend/src/routes/Home/HomeRouter.js
@@ -3,14 +3,14 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 
 /* import { restaurantDetailsRenderer } from './RestaurantDetails/restaurantDetailsRenderer'; */
 import About from './About/About';
-import Diagnoses from './Diagnoses/Diagnoses';
+/* import Diagnoses from './Diagnoses/Diagnoses'; */
 import DiagnosisDetails from './DiagnosisDetails/DiagnosisDetails';
 
 class HomeRouter extends React.Component {
     render() {
         return (
             <Switch>
-                <Route exact={true} path={'/diagnoses'} component={Diagnoses} />
+                {/* <Route exact={true} path={'/diagnoses'} component={Diagnoses} /> */}
                 <Route path='/diagnoses/:diagnosisId' component={DiagnosisDetails} />
                 <Route path={'/about'} component={About} />
                 <Redirect to='/about' />      
