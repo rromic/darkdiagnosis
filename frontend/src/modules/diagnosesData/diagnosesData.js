79243e3b537039432dd5d65891c1852930e6fbaa
croraf <vrafaeli@msn.com>
2020-04-16 00:49:18 +0200

    Add 5 new diagnoses

--- frontend/src/modules/diagnosesData/diagnosesData.js
+++ frontend/src/modules/diagnosesData/diagnosesData.js
@@ -13,6 +13,11 @@ import ondinesCurse from './ondinesCurse.jpg';
 import polydactyly from './polydactyly.jpg';
 import stonemansDisease from './stonemansDisease.jpg';
 import vanishingTwin from './vanishingTwin.jpg';
+import autobrewery from './autobrewery.jpg';
+import aspergerSyndrome from './aspergerSyndrome.jpg';
+import parryRomberg from './parryRomberg.jpg';
+import scleroderma from './scleroderma.jpg';
+import covid from './covid.jpg';
 
 const diagnosesData = [
     {
@@ -43,7 +48,7 @@ const diagnosesData = [
         links: [
             'https://www.ncbi.nlm.nih.gov/pubmed/27194607',
             'https://www.ncbi.nlm.nih.gov/pubmed/9526156',
-            
+
             /* x-ray photo:  */'https://en.m.wikipedia.org/wiki/Polydactyly',
         ]
     },
@@ -102,7 +107,7 @@ const diagnosesData = [
         name: 'Vanishing twin syndrome',
         description: `Vanishing twin syndrome is a phenomenon in which one embryo disappears after documented fetal heart activity in multiple pregnancies. The disappearance usually occurs/happens in early pregnancy, as a consequence of fetal death (the cause is unknown) and the absorption of fetal tissue by the placenta, the other twin or mother. When fetal death occurs after the first trimester, a fetus papyraceus can be formed (a "mummified" and compressed fetus). Vanishing twin syndrome occurs in 20-30% off all multifetal pregnancies and is often asymptomatic. Before high quality ultrasound exams in early pregnancy the syndrome was less frequent and often unnoticed.
 
-        Which anomaly from previous posts can you notice here?`,
+Which anomaly from previous posts can you notice here?`,
         imageUrl: vanishingTwin,
         links: [
             'https://www.ncbi.nlm.nih.gov/m/pubmed/7170918/',
@@ -167,13 +172,89 @@ const diagnosesData = [
         name: 'Blood type change',
         description: `ABO blood group antigenes are the most immunogenic antigenes determining a blood type. They are found on the surface of red blood cells. Although it was believed that a blood type is consistent (permanent) throughout a human’s life, it was reported that in both hematological and solid malignancies (e.g. bladder or lung carcinoma), a blood group can be changed. The loss of ABO antigenes is common in patients with leukemia, in which ABH antigenes are supressed or dimnisihed during the acute leukemic phase. Except blood type change in malignant conditions, research has identified enzymes which can convert other blood types to the “universal“ type O.
         
-        Donate and support 'Krijesnica', @udrugakrijesnica, association for helping children and families facing malignant diseases: 
+Donate and support 'Krijesnica' (Instagram: @udrugakrijesnica), the association for helping children and families facing malignant diseases: 
 IBAN: HR3923600001101481616 (Croatia only)
 http://krijesnica.hr/donacije (for international donations)`,
         imageUrl: bloodTypeChange,
         links: [
-           ' https://www.ncbi.nlm.nih.gov/pmc/articles/PMC5242122/',
+            ' https://www.ncbi.nlm.nih.gov/pmc/articles/PMC5242122/',
             'https://pubs.acs.org/doi/abs/10.1021/ja5116088',
+        ]
+    },
+    {
+        id: 'Auto-brewery Syndrome',
+        name: 'Auto-brewery Syndrome',
+        description: `The condition known as auto-brewery syndrome is a rare medical condition in which ethanol is produced by carbohydrate fermentation in the gastrointestinal tract. Although a person did not or does not consume alcohol, ingested sugars are converted to ethanol in the patient's gut. Saccharomyces cervisiae is a yeast isolated in the GI system, most often causing the gut fermentation syndrome. The quantity of produced alcohol is high enough to cause symptoms which can be the same as someone that has been consuming alcohol. Auto-brewery syndrome is successfully treated by avoiding carbohydrate-rich food, antifungal therapy and probiotics.`,
+        imageUrl: autobrewery,
+        links: [
+            'https://www.ncbi.nlm.nih.gov/books/NBK513346/',
+        ]
+    },
+    {
+        id: 'Scleroderma',
+        name: 'Scleroderma',
+        description: `Scleroderma is an autoimmune multi-system disease characterized by fibrosis of the skin, and possible fibrosis of internal organs in addition to vasculopathy. Scleroderma can be classified into skin localized types and systemic sclerosis; with involment of renal, interstitial lung, diffuse gastrointestinal, and myocardial disease. One of the early symptoms that requires monitoring is Raynaud's phenomenon. in which spasm of arteries causes episodes of discoloration in fingers and toes, after changes in temperature or emotions. It is often an indicator of the future onset of systemic scleroderma. Another symptom can be the significant impairment in the mimicry of the face which thus gives the appearence of a mask. The sunflower is the international symbol for scleroderma as the sunflower turns towards the sun for warmth, smilar to people with scleroderma who usually achieve remission of symptoms in warmer weather.
+        
+More about turtles and sunflowers (Croatian Scleroderma Patients Association): http://huos.hr/udruga`,
+        imageUrl: scleroderma,
+        links: [
+            'https://hrcak.srce.hr/188329',
+            'https://www.ncbi.nlm.nih.gov/m/pubmed/29781586/',
+            'https://www.ncbi.nlm.nih.gov/books/NBK537335/',
+        ]
+    },
+    {
+        id: 'Parry Romberg Syndrome',
+        name: 'Parry Romberg Syndrome',
+        description: `Parry Romberg Syndrome is a rare disorder characterized by atrophy of skin, subcutaneous tissue, muscles, and bones of one side of the face. The disease manifests at one point on the face and slowly progresses to the underlying structures and facial tissues. The etiology of Parry Romberg Syndrome is unknown. Some authors have suggested that PRS is a variant of frontal linear scleroderma with an autoimmune background. Neurological complications such as trigeminal neuralgia, seizures and migraines are common. Progression of facial atrophy is impossible to prevent, however with proper reconstructive and microsurgery interventions, contours of the face can be restored.
+        
+More about Scleroderma on our recent post: https://www.instagram.com/p/B0ZIxudBbmp/?igshid=18255r0utyhvz`,
+        imageUrl: parryRomberg,
+        links: [
+            'https://www.ncbi.nlm.nih.gov/m/pubmed/30851754/',
+            'https://www.sciencedirect.com/science/article/pii/S2214541918300245',
+        ]
+    },
+    {
+        id: 'Asperger Syndrome',
+        name: 'Asperger Syndrome',
+        description: `Asperger syndrome was first described in 1944 when Hans Asperger, an Austrian doctor described four children with “a lack of empathy, little ability to form friendships, one-sided conversation, intense absorption in one particular interest and clumsy movements.” They were each passionately interested in one narrow subject and would talk endlessly about one specific interest. Asperger called them “little professors” because of their ability to talk about their favourite subject in great detail. According to the DSM-5, Asperger syndrome is classified under the single umbrella term of autism spectrum disorder, which includes autism, Asperger syndrome and pervasive developmental disorders. Patients with Asperger's have impaired interaction and nonverbal communication with others, a limited understanding of other’s feelings,  in addition to restricted and repetitive patterns of behavior and interests. Most commonly, persons with Asperger's  have an average level of intelligence if not, even above average levels. It is important to identify Asperger syndrome as early as possible and start with multi-disciplinary treatment. Ignorance of Asperger syndrome can lead to failure in engagement with others socially, depression (especially by adolescents), stress, mood disorders and chronic frustration.
+
+Please check out & donate:
+https://udrugakorakdalje.hr/ 
+HR9723400091110079093 (PBZ) `,
+        imageUrl: aspergerSyndrome,
+        links: [
+            'https://hrcak.srce.hr/82962',
+            'https://www.ncbi.nlm.nih.gov/m/pubmed/15124148/',
+            'https://urn.nsk.hr/urn:nbn:hr:158:523571',
+        ]
+    },
+    {
+        id: 'COVID-19',
+        name: 'COVID-19',
+        description: `COVID-19 is an abbreviation of the  Coronavirus disease, appearing in 2019 and caused by the SARS-CoV-2 coronavirus, belonging to the family Coronaviridae.
+
+Coronaviruses are named for their crown-like spikes on the surface of the virus that attach and enter host cells. 
+
+They appear to have originated in animals, however in some cases, can be transmitted to humans thus causing diseases with fatal respiratory conditions (SARS, MERS, COVID-19 infection). The first case of COVID-19 has been linked to a seafood market in Wuhan, China.
+
+Today the virus is being spread mainly from person to person contact. It is believed that the virus (twice as contagious as the seasonal flu) is transmitted by droplets either directly (by inhaling respiratory droplets) or indirectly (by touching contaminated surfaces where the virus can survive for several hours or even days). People are most contagious when they are sickest, however it is also possible to spread the virus without having symptoms, known as an ASYMPTOMATIC period.
+
+The main symptoms are fever (88.5%), cough (68.6%), myalgia or fatigue (35.8%), expectoration (28.2%) and dyspnea (21.9%). Although most infected patients (80%) quickly recover from the disease, the eldery and populations with chronic diseases are most vulnerable and in such patients, COVID-19 pneumonia can prove fatal.
+
+There is currently (16th April, 2020) no vaccine nor specific and targed drug treatments available. Management consists of supportive care.
+
+Depending on individual country’s healthcare system and capacities, mortality rate from COVID19  currently varies about 3,4%, estimated by WHO (3/3/2020).  In order to decrease the number of cases and slow down the epidemic thus allowing healthcare systems to fight against the virus, it is strongly recommended to decrease and/or stop all unnecessary physical and social contact. Please read and follow the guidelines available below. 
+
+If you suspect having COVID19, please DO NOT visit your doctor, hospital or local emergency room. Instead, call 113 or visit the website: http://koronavirus.hr/
+https://www.who.int/emergencies/diseases/novel-coronavirus-2019/advice-for-public
+
+Protective measures: 
+https://zdravlje.gov.hr/UserDocsImages/2020%20CORONAVIRUS/plakat-za-javne-prostore-COVID.png`,
+        imageUrl: covid,
+        links: [
+
         ]
     },
 ];
