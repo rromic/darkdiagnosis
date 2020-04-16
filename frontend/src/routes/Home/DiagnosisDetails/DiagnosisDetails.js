cd92f90a280a51625f4c1e9996f134d76d553899
croraf <vrafaeli@msn.com>
2020-04-16 18:07:04 +0200

    Move diagnosis title in xs screen size

--- frontend/src/routes/Home/DiagnosisDetails/DiagnosisDetails.js
+++ frontend/src/routes/Home/DiagnosisDetails/DiagnosisDetails.js
@@ -18,9 +18,17 @@ const useStyles = makeStyles(theme => {
         },
         description: {
             margin: '1rem',
-            marginTop: '1rem',
+            marginTop: '0.5rem',
             display: 'flex',
             flexDirection: 'column',
+        },
+        descriptionTitleXs: {
+            fontWeight: 'bold',
+            fontSize: '1.5rem',
+            margin: '0rem auto 0.5rem',
+        },
+        descriptionTitleSm: {
+            display: 'none',
         },
         [theme.breakpoints.up('sm')/* '@media (min-width: 600px)' */]: {
             imageWrapper: {
@@ -29,6 +37,15 @@ const useStyles = makeStyles(theme => {
             },
             description: {
                 marginTop: '0rem',
+            },
+            descriptionTitleSm: {
+                display: 'initial',
+                fontWeight: 'bold',
+                fontSize: '1.4rem',
+                margin: '0.2rem 0rem 1rem', 
+            },
+            descriptionTitleXs: {
+                display: 'none',
             },
             outer: {
                 marginTop: '1rem',
@@ -47,9 +64,10 @@ const DiagnosisDetails = ({ match }) => {
     if (diagnosisData === undefined) {
         return (<div>This diagnosis does not exist :(</div>);
     }
+
     const descriptionComponent = (
         <div className={classes.description}>
-            <div style={{ fontWeight: 'bold', fontSize: '1.4rem', margin: '0.5rem 0rem' }}>{diagnosisData.name}</div>
+            <div className={classes.descriptionTitleSm}>{diagnosisData.name}</div>
             <div style={{ whiteSpace: 'pre-wrap', wordBreak: 'break-word' }}>{diagnosisData.description}</div>
             <div style={{ margin: '2rem 0rem 1rem', fontWeight: 'bold', fontSize: '1.2rem' }}>Links</div>
             {diagnosisData.links.map((link, index) => (
@@ -70,6 +88,7 @@ const DiagnosisDetails = ({ match }) => {
         <div>
             <NavigationRow currentIndex={indexOfDiagnosis} diagnosesData={diagnosesData} />
             <Grid container className={classes.outer}>
+                <div className={classes.descriptionTitleXs}>{diagnosisData.name}</div>
                 <Grid item xs={12} sm={6} className={classes.imageWrapper}>
                     <img alt={diagnosisData.name} src={diagnosisData.imageUrl} width='100%' height='100%' style={{ borderRadius: '5%' }} />
                 </Grid>
