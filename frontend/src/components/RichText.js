ba664f7263276be9c944bcce2ae97cb8242f2266
croraf <vrafaeli@msn.com>
2020-08-24 00:53:49 +0200

    Use RichText library (slate)

---
+++ frontend/src/components/RichText.js
@@ -0,0 +1,179 @@
+import React, { useCallback, useMemo, useState } from 'react';
+import isHotkey from 'is-hotkey'
+import { Editable, withReact, useSlate, Slate } from 'slate-react';
+import { Editor, Transforms, createEditor, } from 'slate';
+import { withHistory } from 'slate-history';
+import FormatBoldIcon from '@material-ui/icons/FormatBold';
+import FormatItalicIcon from '@material-ui/icons/FormatItalic';
+import FormatUnderlinedIcon from '@material-ui/icons/FormatUnderlined';
+import CodeIcon from '@material-ui/icons/Code';
+import LooksOneIcon from '@material-ui/icons/LooksOne';
+import LooksTwoIcon from '@material-ui/icons/LooksTwo';
+import FormatQuoteIcon from '@material-ui/icons/FormatQuote';
+import FormatListNumberedIcon from '@material-ui/icons/FormatListNumbered';
+import FormatListBulletedIcon from '@material-ui/icons/FormatListBulleted';
+
+import Button from './Button';
+import Toolbar from './Toolbar';
+import initialValue from './initialValue';
+
+const HOTKEYS = {
+  'mod+b': 'bold',
+  'mod+i': 'italic',
+  'mod+u': 'underline',
+  'mod+`': 'code',
+}
+
+const LIST_TYPES = ['numbered-list', 'bulleted-list']
+
+const RichTextExample = () => {
+  const [value, setValue] = useState(initialValue);
+  const renderElement = useCallback(props => <Element {...props} />, []);
+  const renderLeaf = useCallback(props => <Leaf {...props} />, []);
+  const editor = useMemo(() => withHistory(withReact(createEditor())), []);
+
+  return (
+    <Slate editor={editor} value={value} onChange={value => setValue(value)}>
+      <Toolbar>
+        <MarkButton format="bold" Icon={FormatBoldIcon} />
+        <MarkButton format="italic" Icon={FormatItalicIcon} />
+        <MarkButton format="underline" Icon={FormatUnderlinedIcon} />
+        <MarkButton format="code" Icon={CodeIcon} />
+        <BlockButton format="heading-one" Icon={LooksOneIcon} />
+        <BlockButton format="heading-two" Icon={LooksTwoIcon} />
+        <BlockButton format="block-quote" Icon={FormatQuoteIcon} />
+        <BlockButton format="numbered-list" Icon={FormatListNumberedIcon} />
+        <BlockButton format="bulleted-list" Icon={FormatListBulletedIcon} />
+      </Toolbar>
+      <Editable
+        renderElement={renderElement}
+        renderLeaf={renderLeaf}
+        placeholder="Enter some rich text…"
+        spellCheck
+        autoFocus
+        onKeyDown={event => {
+          for (const hotkey in HOTKEYS) {
+            if (isHotkey(hotkey, event)) {
+              event.preventDefault()
+              const mark = HOTKEYS[hotkey]
+              toggleMark(editor, mark)
+            }
+          }
+        }}
+      />
+    </Slate>
+  )
+}
+
+const toggleBlock = (editor, format) => {
+  const isActive = isBlockActive(editor, format)
+  const isList = LIST_TYPES.includes(format)
+
+  Transforms.unwrapNodes(editor, {
+    match: n => LIST_TYPES.includes(n.type),
+    split: true,
+  })
+
+  Transforms.setNodes(editor, {
+    type: isActive ? 'paragraph' : isList ? 'list-item' : format,
+  })
+
+  if (!isActive && isList) {
+    const block = { type: format, children: [] }
+    Transforms.wrapNodes(editor, block)
+  }
+}
+
+const toggleMark = (editor, format) => {
+  const isActive = isMarkActive(editor, format)
+
+  if (isActive) {
+    Editor.removeMark(editor, format)
+  } else {
+    Editor.addMark(editor, format, true)
+  }
+}
+
+const isBlockActive = (editor, format) => {
+  const [match] = Editor.nodes(editor, {
+    match: n => n.type === format,
+  })
+
+  return !!match
+}
+
+const isMarkActive = (editor, format) => {
+  const marks = Editor.marks(editor)
+  return marks ? marks[format] === true : false
+}
+
+const Element = ({ attributes, children, element }) => {
+  switch (element.type) {
+    case 'block-quote':
+      return <blockquote {...attributes}>{children}</blockquote>
+    case 'bulleted-list':
+      return <ul {...attributes}>{children}</ul>
+    case 'heading-one':
+      return <h1 {...attributes}>{children}</h1>
+    case 'heading-two':
+      return <h2 {...attributes}>{children}</h2>
+    case 'list-item':
+      return <li {...attributes}>{children}</li>
+    case 'numbered-list':
+      return <ol {...attributes}>{children}</ol>
+    default:
+      return <p {...attributes}>{children}</p>
+  }
+}
+
+const Leaf = ({ attributes, children, leaf }) => {
+  if (leaf.bold) {
+    children = <strong>{children}</strong>
+  }
+
+  if (leaf.code) {
+    children = <code>{children}</code>
+  }
+
+  if (leaf.italic) {
+    children = <em>{children}</em>
+  }
+
+  if (leaf.underline) {
+    children = <u>{children}</u>
+  }
+
+  return <span {...attributes}>{children}</span>
+}
+
+const BlockButton = ({ format, Icon }) => {
+  const editor = useSlate()
+  return (
+    <Button
+      active={isBlockActive(editor, format)}
+      onMouseDown={event => {
+        event.preventDefault()
+        toggleBlock(editor, format)
+      }}
+    >
+      <Icon />
+    </Button>
+  )
+}
+
+const MarkButton = ({ format, Icon }) => {
+  const editor = useSlate();
+  return (
+    <Button
+      active={isMarkActive(editor, format)}
+      onMouseDown={event => {
+        event.preventDefault()
+        toggleMark(editor, format)
+      }}
+    >
+      <Icon />
+    </Button>
+  )
+};
+
+export default RichTextExample;
--- frontend/src/routes/Home/About/About.js
+++ frontend/src/routes/Home/About/About.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo, useState } from 'react';
 import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/styles';
 import MailOutlineIcon from '@material-ui/icons/MailOutline';
@@ -6,113 +6,115 @@ import FacebookIcon from '@material-ui/icons/Facebook';
 import InstagramIcon from '@material-ui/icons/Instagram';
 import { useSelector, } from 'react-redux';
 import DiagnosisThumbnail from '../DiagnosisThumbnail';
+import RichText from 'components/RichText';
 
 const useStyles = makeStyles(theme => {
-    return ({
-        outer: {
-            textAlign: 'center',
-            width: '700px',
-            maxWidth: '100%',
-            backgroundColor: 'white',
-            borderRadius: '30px',
-            padding: '0rem 1rem',
-            margin: '1rem 0rem',
-        },
-        gallery: {
-            display: 'flex',
-            flexDirection: 'row',
-            flexWrap: 'wrap',
-        },
-        contactItems: {
-            marginBottom: '0rem',
-            display: 'flex',
-            flexDirection: 'row',
-            alignItems: 'center',
-            justifyContent: 'center',
-        },
-        [theme.breakpoints.up('sm')/* '@media (min-width: 600px)' */]: {
-            outer: {
-                padding: '0px 50px',
-                margin: '2rem 0rem 1rem',
-            },
-        },
-    });
+  return ({
+    outer: {
+      width: '700px',
+      maxWidth: '100%',
+      backgroundColor: 'white',
+      borderRadius: '30px',
+      padding: '0rem 1rem',
+      margin: '1rem 0rem',
+    },
+    gallery: {
+      display: 'flex',
+      flexDirection: 'row',
+      flexWrap: 'wrap',
+    },
+    contactItems: {
+      marginBottom: '0rem',
+      display: 'flex',
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    [theme.breakpoints.up('sm')/* '@media (min-width: 600px)' */]: {
+      outer: {
+        padding: '0px 50px',
+        margin: '2rem 0rem 1rem',
+      },
+    },
+  });
 });
 
 const About = () => {
-    const classes = useStyles();
-    const diagnosesData = useSelector(state => state.diagnosesData);
+  const classes = useStyles();
+  const diagnosesData = useSelector(state => state.diagnosesData);
 
-    return (
-        <Grid container className={classes.outer}>
-            <Grid item xs={12} md={12} style={{ textAlign: 'center', }}>
-                <div style={{ marginTop: '1rem', marginBottom: '1.5rem' }}>
-                    <div style={{ fontSize: '1.4rem', marginBottom: '1.5rem', fontWeight: 'bold' }}>About the project</div>
-                    <div style={{
-                        display: 'flex',
-                        flexDirection: 'column',
-                        justifyContent: 'space-between',
-                    }}>
-                        <div style={{ marginBottom: '1rem', textAlign: 'justify' }}>
-                            {`Dark diagnosis is a project which combines illustrations with medical descriptions of diseases. The idea is to raise awareness and inform a wider audience about rare diagnoses, and also to promote and support science.`}
-                        </div>
-                        <div style={{ marginBottom: '1rem', textAlign: 'justify' }}>
-                            {`Below every illustration and description you can find links to evidence based medical articles. In addition, every topic is connected to the respective Croatian association that helps patients with the described disease.`}
-                        </div>
-                        <div style={{ marginBottom: '1rem', textAlign: 'justify' }}>
-                            {`As a future goal of the project we would like to establish a charity art exhibition with the availability of merchandise for the donation to humanitarian causes.`}
-                        </div>
-                    </div>
-                </div>
-            </Grid>
+  return (
+    <Grid container className={classes.outer}>
+      <Grid item xs={12} md={12}>
+        <div style={{ marginTop: '1rem', marginBottom: '1.5rem' }}>
+          <RichText />
 
-            <Grid item xs={12} md={12} style={{ textAlign: 'center' }}>
-                <div style={{ marginTop: '0rem', marginBottom: '3.5rem' }}>
-                    <div style={{ fontSize: '1.4rem', marginBottom: '1.5rem', fontWeight: 'bold' }}>Gallery</div>
-                    <div className={classes.gallery}>
-                        {diagnosesData.map(diagnosisData => (
-                            <div 
-                                key={diagnosisData.id}
-                                style={{
-                                    margin: '0.5rem 0.41% 0.5rem',
-                                    width: '19%',
-                                    display: 'flex',
-                                }}
-                            >
-                                <DiagnosisThumbnail key={diagnosisData.id} diagnosisData={diagnosisData} />
-                            </div>
-                        ))}
-                    </div>
-                </div>
-            </Grid>
+          <div style={{ fontSize: '1.4rem', marginBottom: '1.5rem', fontWeight: 'bold', textAlign: 'center' }}>About the project</div>
+          <div style={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'space-between',
+          }}>
+            <div style={{ marginBottom: '1rem', textAlign: 'justify' }}>
+              {`Dark diagnosis is a project which combines illustrations with medical descriptions of diseases. The idea is to raise awareness and inform a wider audience about rare diagnoses, and also to promote and support science.`}
+            </div>
+            <div style={{ marginBottom: '1rem', textAlign: 'justify' }}>
+              {`Below every illustration and description you can find links to evidence based medical articles. In addition, every topic is connected to the respective Croatian association that helps patients with the described disease.`}
+            </div>
+            <div style={{ marginBottom: '1rem', textAlign: 'justify' }}>
+              {`As a future goal of the project we would like to establish a charity art exhibition with the availability of merchandise for the donation to humanitarian causes.`}
+            </div>
+          </div>
+        </div>
+      </Grid>
+
+      <Grid item xs={12} md={12} style={{ textAlign: 'center' }}>
+        <div style={{ marginTop: '0rem', marginBottom: '3.5rem' }}>
+          <div style={{ fontSize: '1.4rem', marginBottom: '1.5rem', fontWeight: 'bold' }}>Gallery</div>
+          <div className={classes.gallery}>
+            {diagnosesData.map(diagnosisData => (
+              <div
+                key={diagnosisData.id}
+                style={{
+                  margin: '0.5rem 0.41% 0.5rem',
+                  width: '19%',
+                  display: 'flex',
+                }}
+              >
+                <DiagnosisThumbnail key={diagnosisData.id} diagnosisData={diagnosisData} />
+              </div>
+            ))}
+          </div>
+        </div>
+      </Grid>
 
 
 
-            <Grid item xs={12} md={12} style={{ textAlign: 'center' }}>
-                <div style={{ marginBottom: '2rem' }}>
-                    <div style={{ fontSize: '1.4rem', marginBottom: '1rem', fontWeight: 'bold' }}>Contact us</div>
-                    <div style={{
-                        display: 'flex',
-                        flexDirection: 'column',
-                        justifyContent: 'space-between',
-                    }}>
-                        <div className={classes.contactItems}>
-                            <FacebookIcon style={{ margin: '0.5rem', color: '#673ab7' }} />
-                            <a href='https://www.facebook.com/darkdiagnosis/'>facebook.com/darkdiagnosis</a>
-                        </div>
-                        <div className={classes.contactItems}>
-                            <InstagramIcon style={{ margin: '0.5rem' }} />
-                            <a href='https://www.instagram.com/darkdiagnosis/'>instagram.com/darkdiagnosis</a>
-                        </div>
-                        <div className={classes.contactItems}>
-                            <MailOutlineIcon style={{ margin: '0.5rem' }} />
-                            <span>darkdiagnosis1@gmail.com</span>
-                        </div>
-                    </div>
-                </div>
-            </Grid>
+      <Grid item xs={12} md={12} style={{ textAlign: 'center' }}>
+        <div style={{ marginBottom: '2rem' }}>
+          <div style={{ fontSize: '1.4rem', marginBottom: '1rem', fontWeight: 'bold' }}>Contact us</div>
+          <div style={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'space-between',
+          }}>
+            <div className={classes.contactItems}>
+              <FacebookIcon style={{ margin: '0.5rem', color: '#673ab7' }} />
+              <a href='https://www.facebook.com/darkdiagnosis/'>facebook.com/darkdiagnosis</a>
+            </div>
+            <div className={classes.contactItems}>
+              <InstagramIcon style={{ margin: '0.5rem' }} />
+              <a href='https://www.instagram.com/darkdiagnosis/'>instagram.com/darkdiagnosis</a>
+            </div>
+            <div className={classes.contactItems}>
+              <MailOutlineIcon style={{ margin: '0.5rem' }} />
+              <span>darkdiagnosis1@gmail.com</span>
+            </div>
+          </div>
+        </div>
+      </Grid>
 
-            {/* <Grid item xs={12} md={12} style={{textAlign: 'center'}}>
+      {/* <Grid item xs={12} md={12} style={{textAlign: 'center'}}>
                 <div style={{marginBottom: '4rem'}}>
                     <div style={{fontSize: '1.4rem', marginBottom: '1rem', fontWeight: 'bold'}}>Lokacija</div>
                     <div style={{
@@ -126,8 +128,8 @@ const About = () => {
                     </div>
                 </div>
             </Grid> */}
-        </Grid>
-    );
+    </Grid>
+  );
 };
 
 export default About;
