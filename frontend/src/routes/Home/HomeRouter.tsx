e9d4c0c337455df6e26e32e178d06eea23d87320
croraf <vrafaeli@msn.com>
2020-12-05 16:12:39 +0100

    Deffer loading of diagnoses details route

--- frontend/src/routes/Home/HomeRouter.tsx
+++ frontend/src/routes/Home/HomeRouter.tsx
@@ -1,14 +1,15 @@
-import React from 'react';
+import React, { lazy } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 import About from './About/About';
-/* import Diagnoses from './Diagnoses/Diagnoses'; */
-import DiagnosisDetails from './DiagnosisDetails/DiagnosisDetails';
+
+//import DiagnosisDetails from './DiagnosisDetails/DiagnosisDetails';
+const DiagnosisDetails = lazy(() => import('./DiagnosisDetails/DiagnosisDetails'));
+
 
 const HomeRouter = () => {
   return (
     <Switch>
-      {/* <Route exact={true} path={'/diagnoses'} component={Diagnoses} /> */}
       <Route path='/diagnoses/:diagnosisId' component={DiagnosisDetails} />
       <Route exact={true} path={'/'} component={About} />
       <Redirect to='/' />
