bdeebbcfc73a011350ecf9d99f4e4052c32d0689
croraf <vrafaeli@msn.com>
2020-12-05 02:54:50 +0100

    Convert auth redux slice to redux-toolkit

--- frontend/src/modules/auth.ts
+++ frontend/src/modules/auth.ts
@@ -1,21 +1,24 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import firebase from 'firebase';
 
-const login = (user: firebase.User | null) => (dispatch: any) => {
-  const payload = user === null ? null : { displayName: user.displayName, email: user.email };
-  dispatch({ type: 'auth/login', payload });
-};
+
+type Auth = { displayName: string | null, email: string | null } | null;
 
 const authSlice = createSlice({
   name: 'auth',
-  initialState: null as firebase.User | null,
+  initialState: null as Auth,
   reducers: {
-    login: (state, action) => {
-      return action.payload;
+    login: {
+      reducer: (state, action: PayloadAction<Auth>) => {
+        return action.payload;
+      },
+      prepare: (user: firebase.User | null) => {
+        return { payload: user === null ? null : { displayName: user.displayName, email: user.email } };
+      }
     }
   },
 });
 
-export { login };
+export const { login } = authSlice.actions;
 export default authSlice.reducer;
 
